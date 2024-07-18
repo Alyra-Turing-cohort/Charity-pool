f82302bf49006fbc3b1225a958001d185aece7fc
Yassine <yadine.yassine7@gmail.com>
2024-07-18 23:46:25 +0200

    setup basic ui

---
+++ app/components/Contribute.tsx
@@ -0,0 +1,100 @@
+"use client";
+import * as React from "react";
+import Backdrop from "@mui/material/Backdrop";
+import Box from "@mui/material/Box";
+import Modal from "@mui/material/Modal";
+import Fade from "@mui/material/Fade";
+import Button from "@mui/material/Button";
+import Typography from "@mui/material/Typography";
+import Slider from "@mui/material/Slider";
+import { Wallet } from "@mui/icons-material";
+import { useState } from "react";
+import { useWallet } from "@solana/wallet-adapter-react";
+
+const style = {
+  position: "absolute" as "absolute",
+  top: "50%",
+  left: "50%",
+  transform: "translate(-50%, -50%)",
+  width: 400,
+  bgcolor: "background.paper",
+  border: "2px solid #000",
+  boxShadow: 24,
+  borderRadius: 5,
+  p: 4,
+};
+
+
+export default function TransitionsModal() {
+  const [sliderDefaultValue, setSliderDefaultValue] = useState(1);
+  const [open, setOpen] = React.useState(false);
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
+  const wallet = useWallet();
+
+  const valueLabelFormat = (value: number) => {
+    setSliderDefaultValue(value);
+    return `${value} SOL`;
+  };
+
+  const contribute = () => {
+    console.log(sliderDefaultValue);
+  };
+
+  return (
+    <div className="flex flex-row items-start justify-start gap-8">
+      <Button className="btn glow-on-hover" onClick={handleOpen}>
+        Contribute
+      </Button>
+      <Modal
+        aria-labelledby="transition-modal-title"
+        aria-describedby="transition-modal-description"
+        open={open}
+        onClose={handleClose}
+        closeAfterTransition
+        BackdropComponent={Backdrop}
+        BackdropProps={{
+          timeout: 500,
+        }}
+      >
+        <Fade in={open}>
+          <Box sx={style}>
+            <Typography id="transition-modal-title" variant="h6" component="h2">
+              Hey contributor 👋
+            </Typography>
+            <Typography id="transition-modal-description" sx={{ mt: 2, mb: 2 }}>
+              We encourage you to contribute to this pool by helping them.
+            </Typography>
+
+            <Box sx={{ width: 300 }}>
+              <Slider
+                aria-label="Temperature"
+                defaultValue={sliderDefaultValue}
+                getAriaValueText={valueLabelFormat}
+                valueLabelDisplay="auto"
+                step={0.5}
+                marks
+                min={0.5}
+                max={20}
+              />
+            </Box>
+
+            {wallet.connected && (
+              <>
+                <Button
+                  variant="outlined"
+                  color="secondary"
+                  endIcon={<Wallet />}
+                  onClick={contribute}
+                >
+                  Contribute
+                </Button>
+                <span>SOL {sliderDefaultValue} </span>
+              </>
+            )}
+          </Box>
+        </Fade>
+      </Modal>
+    </div>
+  );
+}
---
+++ app/components/CreatePoolForm.tsx
@@ -0,0 +1,130 @@
+"use client";
+import * as React from "react";
+import Backdrop from "@mui/material/Backdrop";
+import Box from "@mui/material/Box";
+import Modal from "@mui/material/Modal";
+import Fade from "@mui/material/Fade";
+import Button from "@mui/material/Button";
+import Typography from "@mui/material/Typography";
+import Slider from "@mui/material/Slider";
+import { Wallet } from "@mui/icons-material";
+import { useState } from "react";
+import { useWallet } from "@solana/wallet-adapter-react";
+import { InputLabel, MenuItem, Select, SelectChangeEvent, TextField } from "@mui/material";
+
+const style = {
+  position: "absolute" as "absolute",
+  top: "50%",
+  left: "50%",
+  transform: "translate(-50%, -50%)",
+  width: 400,
+  bgcolor: "background.paper",
+  border: "2px solid #000",
+  boxShadow: 24,
+  borderRadius: 5,
+  p: 4,
+};
+
+const solanaLabs = "Solana labs";
+const alyraAlumni = "Alyra alumni";
+const antiCapitalism = "Anti capitalism";
+
+export default function CreatePoolForm() {
+  const [open, setOpen] = React.useState(false);
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
+  const [donation, setDonation] = useState("");
+  const [sliderDefaultValue, setSliderDefaultValue] = useState(1);
+
+  const wallet = useWallet();
+
+  const createPool = () => {
+    console.log(sliderDefaultValue + ' SOL @ ' + donation);
+  };
+
+  const handleChange = (event: SelectChangeEvent) => {
+    setDonation(event.target.value as string);
+  };
+
+  const handleTitleChange = (event: SelectChangeEvent) => {
+    console.log(event.target.value as string);
+  };
+
+  const valueLabelFormat = (value: number) => {
+    setSliderDefaultValue(value);
+    return `${value} SOL`;
+  };
+
+  return (
+    <div className="flex flex-row items-start justify-start gap-8">
+      <Button className="btn glow-on-hover" onClick={handleOpen}>
+        Create a new pool
+      </Button>
+      <Modal
+        aria-labelledby="transition-modal-title"
+        aria-describedby="transition-modal-description"
+        open={open}
+        onClose={handleClose}
+        closeAfterTransition
+        BackdropComponent={Backdrop}
+        BackdropProps={{
+          timeout: 500,
+        }}
+      >
+        <Fade in={open}>
+          <Box sx={style}>
+            <Typography id="transition-modal-title" variant="h6" component="h2">
+              Hey contributor!
+            </Typography>
+            <Typography id="transition-modal-description" sx={{ mt: 2, mb: 2 }}>
+              You rock 👋
+            </Typography>
+
+
+            <TextField id="standard-basic" label="Pool title" variant="standard" onChange={handleTitleChange}/>
+
+            <InputLabel id="demo-simple-select-label">Donation</InputLabel>
+            <Select
+              labelId="demo-simple-select-label"
+              id="demo-simple-select"
+              value={donation}
+              label="Donation"
+              onChange={handleChange}
+            >
+              <MenuItem value={solanaLabs}>{solanaLabs}</MenuItem>
+              <MenuItem value={alyraAlumni}>{alyraAlumni}</MenuItem>
+              <MenuItem value={antiCapitalism}>{antiCapitalism}</MenuItem>
+            </Select>
+
+            <Box sx={{ width: 300 }}>
+              <Slider
+                aria-label="Temperature"
+                defaultValue={sliderDefaultValue}
+                getAriaValueText={valueLabelFormat}
+                valueLabelDisplay="auto"
+                step={0.5}
+                marks
+                min={0.5}
+                max={20}
+              />
+            </Box>
+
+            <Box>
+              <>
+                <Button
+                  variant="outlined"
+                  color="secondary"
+                  endIcon={<Wallet />}
+                  onClick={createPool}
+                >
+                  Create new pool
+                </Button>
+                <span>SOL {sliderDefaultValue} </span>
+              </>
+            </Box>
+          </Box>
+        </Fade>
+      </Modal>
+    </div>
+  );
+}
