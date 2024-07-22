9c2467a65ecefa8d31077d8e9dda42eca42128f1
Andrij David <andrijdavid@gmail.com>
2024-07-22 17:10:06 +0200

    add more tests

--- tests/charity-pool.ts
+++ tests/charity-pool.ts
@@ -1,13 +1,13 @@
 import * as anchor from "@coral-xyz/anchor";
-import { Program } from "@coral-xyz/anchor";
-import { CharityPool } from "../target/types/charity_pool";
+import {Program} from "@coral-xyz/anchor";
+import {CharityPool} from "../target/types/charity_pool";
 import {
     LAMPORTS_PER_SOL,
     PublicKey,
     SystemProgram,
     Keypair
 } from "@solana/web3.js";
-import { assert } from "chai";
+import {assert} from "chai";
 
 describe("charity-pool", () => {
     // Configure the client to use the local cluster.
@@ -74,6 +74,9 @@ describe("charity-pool", () => {
         console.log("Your transaction signature", tx);
     });
 
+    /**
+     * Pool creation test
+     * */
     it("Pool Created", async () => {
         const createPoolParams = {
             pool_name: "Test Pool",
@@ -104,6 +107,30 @@ describe("charity-pool", () => {
         const contributions = poolAccount.contributions;
         assert.ok(contributions.length > 0, "Initial contrib should be recorded");
     });
+
+    it("Create Pool with Missing Fields", async () => {
+        try {
+            await program.methods.createPool("",
+                donationKeypair.publicKey,
+                new anchor.BN(POOL_INITIAL_FUNDING)
+            )
+                .accounts({
+                    pool: poolPda,
+                    donation: donationKeypair.publicKey,
+                    creator: poolCreator.publicKey,
+                    systemProgram: SystemProgram.programId,
+                })
+                .signers([poolCreator])
+                .rpc();
+
+        } catch (error) {
+            assert.ok(error, "Should throw error for empty pool name");
+        }
+    });
+
+    /**
+     * Contribution test
+     * */
 
     it("Contributes to the Pool", async () => {
         const contributionAmount = new anchor.BN(CONTRIBUTION_AMOUNT);
@@ -139,6 +166,144 @@ describe("charity-pool", () => {
         console.log("Contribution recorded in pool: ", poolAccount.contributions);
     });
 
+    it("Multiple Contributions", async () => {
+        const NUM_CONTRIBUTORS = 10;
+        const contributors = [];
+
+        // Create the required number of contributors and airdrop some SOL to them
+        for (let i = 0; i < NUM_CONTRIBUTORS; i++) {
+            const contributor = Keypair.generate();
+            contributors.push(contributor);
+
+            await provider.connection.confirmTransaction(
+                await provider.connection.requestAirdrop(
+                    contributor.publicKey,
+                    10 * LAMPORTS_PER_SOL // Assuming each contributor gets 10 SOL for the test
+                ),
+                "confirmed"
+            );
+        }
+
+        const contributionAmount = new anchor.BN(CONTRIBUTION_AMOUNT + 1);
+
+        for (const contributor of contributors) {
+            await program.methods.contribute(contributionAmount)
+                .accounts({
+                    pool: poolPda,
+                    creator: poolCreator.publicKey,
+                    contributor: contributor.publicKey,
+                    donation: donationKeypair.publicKey,
+                    systemProgram: SystemProgram.programId,
+                })
+                .signers([contributor])
+                .rpc();
+        }
+
+        const poolAccount = await program.account.pool.fetch(poolPda);
+        const contributions = poolAccount.contributions;
+        assert.ok(contributions.length >= NUM_CONTRIBUTORS, `There should be at least ${NUM_CONTRIBUTORS} contributions recorded.`);
+    });
+
+    it("Contributes with Insufficient Funds", async () => {
+        const insufficientContributorKeypair = Keypair.generate();
+        await provider.connection.requestAirdrop(insufficientContributorKeypair.publicKey, 0.5 * LAMPORTS_PER_SOL);
+
+        const contributionAmount = new anchor.BN(CONTRIBUTION_AMOUNT);
+
+        try {
+            await program.methods.contribute(contributionAmount)
+                .accounts({
+                    pool: poolPda,
+                    creator: poolCreator.publicKey,
+                    contributor: insufficientContributorKeypair.publicKey,
+                    donation: donationKeypair.publicKey,
+                    systemProgram: SystemProgram.programId,
+                })
+                .signers([insufficientContributorKeypair])
+                .rpc();
+        } catch (error) {
+            assert.ok(error, "Should throw an error for insufficient funds");
+        }
+    });
+
+    it("Negative Contribution Amount", async () => {
+        const negativeContributionAmount = new anchor.BN(-CONTRIBUTION_AMOUNT);
+
+        try {
+            await program.methods.contribute(negativeContributionAmount)
+                .accounts({
+                    pool: poolPda,
+                    creator: poolCreator.publicKey,
+                    contributor: contributorKeypair.publicKey,
+                    donation: donationKeypair.publicKey,
+                    systemProgram: SystemProgram.programId,
+                })
+                .signers([contributorKeypair])
+                .rpc();
+        } catch (error) {
+            assert.ok(error, "Should throw an error for negative contribution amounts");
+        }
+    });
+
+
+    /**
+     * Pre-drwaing distribution test
+     * */
+    it("Distributes funds without drawing winner", async () => {
+        try {
+            await program.methods.distributeFunds()
+                .accounts({
+                    pool: poolPda,
+                    poolVault: poolVaultPda,
+                    providedWinner: contributorKeypair.publicKey,
+                    creator: poolCreator.publicKey,
+                    donation: donationKeypair.publicKey,
+                    systemProgram: SystemProgram.programId,
+                })
+                .signers([contributorKeypair])
+                .rpc();
+        } catch (error) {
+            assert.ok(error, "Should throw an error if no winner has been drawn");
+        }
+    });
+
+
+    /**
+     * Drawing
+     * */
+    it("Draw Winner Before Pool Ends", async () => {
+        try {
+            await program.methods.drawWinner()
+                .accounts({
+                    pool: poolPda,
+                    creator: poolCreator.publicKey,
+                    systemProgram: SystemProgram.programId,
+                })
+                .signers([poolCreator])
+                .rpc();
+        } catch (error) {
+            assert.ok(error, "Should throw an error if pool has not ended");
+        }
+    });
+
+    it("Draw Winner", async () => {
+        await program.methods.drawWinner()
+            .accounts({
+                pool: poolPda,
+                creator: poolCreator.publicKey,
+                systemProgram: SystemProgram.programId,
+            })
+            .signers([poolCreator])
+            .rpc();
+
+        const poolAccount = await program.account.pool.fetch(poolPda);
+        assert.isNotNull(poolAccount.winner, "Winner should be drawn");
+    });
+
+    /**
+     * Distribution test
+     *
+     * */
 
     it("Distributes funds", async () => {
         // Draw winner
@@ -204,5 +369,50 @@ describe("charity-pool", () => {
         console.log("Funds distributed successfully.");
     });
 
+    it("Pool Already Claimed", async () => {
+        await program.methods.drawWinner()
+            .accounts({
+                pool: poolPda,
+                creator: poolCreator.publicKey,
+                systemProgram: SystemProgram.programId,
+            })
+            .signers([poolCreator])
+            .rpc();
+
+        // Now distribute funds to claim the pool
+        const poolAccount = await program.account.pool.fetch(poolPda);
+
+        await program.methods.distributeFunds()
+            .accounts({
+                pool: poolPda,
+                poolVault: poolVaultPda,
+                providedWinner: poolAccount.winner,
+                creator: poolCreator.publicKey,
+                donation: donationKeypair.publicKey,
+                systemProgram: SystemProgram.programId,
+            })
+            .signers([poolAccount.winner])
+            .rpc();
+
+        const updatedPoolAccount = await program.account.pool.fetch(poolPda);
+        assert.isTrue(updatedPoolAccount.claimed, "Pool should be marked as claimed");
+
+        // Attempt to distribute funds again which should fail
+        try {
+            await program.methods.distributeFunds()
+                .accounts({
+                    pool: poolPda,
+                    poolVault: poolVaultPda,
+                    providedWinner: poolAccount.winner,
+                    creator: poolCreator.publicKey,
+                    donation: donationKeypair.publicKey,
+                    systemProgram: SystemProgram.programId,
+                })
+                .signers([poolAccount.winner])
+                .rpc();
+        } catch (error) {
+            assert.ok(error, "Should throw an error if trying to distribute funds for already claimed pool");
+        }
+    });
 
 });
